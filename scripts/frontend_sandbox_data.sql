3f862f42c3a1b90fdc0d92eeb40651ffbc01ac6b
Sergey Shambir <sshambir@yandex.ru>
2018-06-12 22:59:59 +0300

    Updated dependecies install script and sandbox model

--- scripts/frontend_sandbox_data.sql
+++ scripts/frontend_sandbox_data.sql
@@ -4,5 +4,5 @@ INSERT INTO contest (title, start_time, end_time) VALUES ('Test Contest', '2018-
 SELECT @contest_id := id FROM contest WHERE title='Test Contest';
 INSERT INTO assignment (contest_id, title, article) VALUES (@contest_id, 'A+B Problem', 'Solve A+B problem using C++ or PASCAL');
 INSERT INTO assignment (contest_id, title, article) VALUES (@contest_id, 'A*B Problem', 'Solve A*B problem using C++ or PASCAL');
-INSERT INTO user (username, password, roles) VALUES ('Martin', '2018', 'student');
-INSERT INTO user (username, password, roles) VALUES ('Piter', '2018', 'judge');
+INSERT INTO user (username, password, roles, active_contest_id) VALUES ('Martin', '2018', 'student', @contest_id);
+INSERT INTO user (username, password, roles, active_contest_id) VALUES ('Piter', '2018', 'judge', @contest_id);
