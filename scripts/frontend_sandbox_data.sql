f013d7308862ee4c09a84b77fa6d7f2ccd09c4f9
Sergey Shambir <sshambir@yandex.ru>
2018-06-12 21:32:55 +0300

    Updated MySQL tables model

---
+++ scripts/frontend_sandbox_data.sql
@@ -0,0 +1,8 @@
+
+USE `psjudge_frontend`;
+INSERT INTO contest (title, start_time, end_time) VALUES ('Test Contest', '2018-10-10 10:00:00', '2018-10-10 12:00:00');
+SELECT @contest_id := id FROM contest WHERE title='Test Contest';
+INSERT INTO assignment (contest_id, title, article) VALUES (@contest_id, 'A+B Problem', 'Solve A+B problem using C++ or PASCAL');
+INSERT INTO assignment (contest_id, title, article) VALUES (@contest_id, 'A*B Problem', 'Solve A*B problem using C++ or PASCAL');
+INSERT INTO user (username, password, roles) VALUES ('Martin', '2018', 'student');
+INSERT INTO user (username, password, roles) VALUES ('Piter', '2018', 'judge');
